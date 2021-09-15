a8290e06b6175c7e0d91a7267b3213890094221d
gensahilsingh <70997399+gensahilsingh@users.noreply.github.com>
2021-09-15 10:21:23 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function add() {
+    var result1 = Number(document.getElementById("first_num").value) + Number(document.getElementById("second_num").value);
+    window.alert(result1);
+}
+
+function sub() {
+    var result2 = Number(document.getElementById("first_num").value) - Number(document.getElementById("second_num").value);
+    window.alert(result2);
+}
+
+function multi() {
+    var result3 = Number(document.getElementById("first_num").value) * Number(document.getElementById("second_num").value);
+    window.alert(result3);
+}
+
+function divis() {
+    var result4 = Number(document.getElementById("first_num").value) / Number(document.getElementById("second_num").value);
+    window.alert(result4);
+}
